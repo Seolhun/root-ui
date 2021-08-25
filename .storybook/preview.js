d9967e8da4707209c056bf5015635ef773567896
Seolhun <shun10114@gmail.com>
2021-08-25 12:24:56 +0900

    chore(test) : fix jest configuration

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import { addDecorator } from '@storybook/react';
-import '../src/assets/app.scss';
-
 export const parameters = {
   layout: 'fullscreen',
-  actions: {
-    argTypesRegex: '^on[A-Z].*'
-  },
   controls: {
     expanded: true
   },
+  actions: {
+    argTypesRegex: '^on[A-Z].*'
+  },
+  options: {
+    showRoots: true
+  }
 };
-
-const AppDecorator = (storyFn) => (
-  <div>{storyFn()}</div>
-);
-
-addDecorator(AppDecorator);
--- build/webpack.common.js
+++ build/webpack.common.js
@@ -36,7 +36,6 @@ module.exports = {
       },
     ],
   },
-
   performance: {
     hints: false,
   },
