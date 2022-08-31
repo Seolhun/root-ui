bdf7cab14a02dfe7980d08776caf47dba6a3a1af
Seolhun <shun10114@gmail.com>
2022-08-31 11:15:55 +0900

    feat(hooks): update hooks for ui

--- src/hooks/useEvent.ts
+++ src/hooks/useEvent.ts
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useLatestValue } from './useLatestValue';
 
-export function useEvent<F extends (...args: any[]) => any, P extends any[] = Parameters<F>, R = ReturnType<F>>(
+type EventCallback = <Arg = any, Res = any>(...args: Arg[]) => Res;
+
+export function useEvent<F extends EventCallback, P extends Parameters<F> = Parameters<F>, R = ReturnType<F>>(
   cb: (...args: P) => R,
 ) {
   const cache = useLatestValue(cb);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,6 @@ export * from './disposables';
 export * from './FocusManagements';
 export * from './getOwnerDocument';
 export * from './isServer';
+export * from './match';
 export * from './render';
 export * from './toMicrotask';
