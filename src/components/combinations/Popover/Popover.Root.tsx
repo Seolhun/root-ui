2e10c41f526ee067c978f3e8b35f72578cc124e0
Seolhun <shun10114@gmail.com>
2022-12-29 16:54:52 +0900

    refactor(popover): add popover render args children type

--- src/components/combinations/Popover/Popover.Root.tsx
+++ src/components/combinations/Popover/Popover.Root.tsx
@@ -6,13 +6,22 @@ const CLASSNAME = 'Root__Popover';
 type ElementType = HTMLDivElement;
 type ElementProps = React.HTMLAttributes<ElementType>;
 
-export interface PopoverRootProps extends ElementProps {}
+export interface PopoverRootProps extends Omit<ElementProps, 'children'> {
+  children: ((args: PopoverRootRenderArgs) => JSX.Element) | JSX.Element;
+}
+
+export interface PopoverRootRenderArgs {
+  open: boolean;
+  close: (
+    focusableElement?: HTMLElement | React.MutableRefObject<HTMLElement | null> | React.MouseEvent<HTMLElement>,
+  ) => void;
+}
 
 export const PopoverRoot = React.forwardRef<ElementType, PopoverRootProps>(
   ({ className, children, ...others }, ref) => {
     return (
       <Popover {...others} ref={ref} className={clsx(CLASSNAME, className, 'relative')}>
-        {children}
+        {typeof children === 'function' ? ({ close, open }) => children({ close, open }) : children}
       </Popover>
     );
   },
