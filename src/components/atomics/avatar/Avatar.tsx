c0878c2da4020b23335c383b5561a67acc6bfaec
Seolhun <shun10114@gmail.com>
2023-02-13 22:09:38 +0900

    feat(avatar): add avatar

---
+++ src/components/atomics/avatar/Avatar.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import clsx from 'clsx';
+import { RootScale, toScaleMatch } from '../../../system';
+
+const CLASSNAME = 'Root__Avatar';
+type ElementType = HTMLImageElement;
+type ElementProps = React.ImgHTMLAttributes<ElementType>;
+
+export interface AvatarProps {
+  /**
+   * Set this to change scale
+   * @default md
+   */
+  scale?: RootScale;
+
+  /**
+   * To handle image error when it has error.
+   */
+  emptySrc?: string;
+}
+
+export const Avatar = React.forwardRef<ElementType, AvatarProps & ElementProps>(
+  ({ className, scale = 'md', alt = 'avatar', onError, emptySrc, ...rests }, ref) => {
+    const onImageError = React.useCallback(
+      (e: React.UIEvent<ElementType>) => {
+        onError?.(e);
+        if (emptySrc) {
+          e.currentTarget.onerror = null;
+          e.currentTarget.src = emptySrc;
+        }
+      },
+      [onError, emptySrc],
+    );
+
+    return (
+      <picture className={clsx(CLASSNAME, className, 'inline-block')}>
+        <img
+          {...rests}
+          ref={ref}
+          className={clsx(
+            'rounded-full',
+            toScaleMatch({
+              xs: () => 'size-xs',
+              sm: () => 'size-sm',
+              md: () => 'size-md',
+              lg: () => 'size-lg',
+              xl: () => 'size-xl',
+            })(scale),
+          )}
+          alt={alt}
+          onError={emptySrc ? undefined : onImageError}
+        />
+      </picture>
+    );
+  },
+);
