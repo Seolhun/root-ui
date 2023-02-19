3289d4f19522ff9d215b55a95ce009dc5a4d991e
Seolhun <shun10114@gmail.com>
2023-02-19 12:58:23 +0900

    refactor: update autocomplete styles

--- src/components/combinations/AutoComplete/AutoComplete.Styled.ts
+++ src/components/combinations/AutoComplete/AutoComplete.Styled.ts
@@ -1,33 +1,27 @@
 import clsx from 'clsx';
 
-export const InputWrapper = clsx('flex items-center', 'w-full', 'bg-cream dark:bg-space', 'outline-0');
+export const InputWrapper = clsx(
+  'flex items-center',
+  'w-full',
+  'bg-cream-1 dark:bg-space-1',
+  'text-space-2 dark:text-cream-2',
+  'outline-0',
+);
 
 export const OptionGroupWrapper = clsx('relative');
 
 export const OptionGroup = clsx(
   'absolute',
-  'z-10',
   'flex flex-1 flex-col',
   'w-full',
-  // 'max-h-120',
-  'bg-cream dark:bg-space',
+  'bg-cream-1 dark:bg-space-1',
   'rounded-md shadow-md',
   'overflow-y-auto',
+  'z-10',
 );
 
-export const BaseOption = clsx(
-  'flex items-center flex-1',
-  'w-full',
-  'bg-cream dark:bg-space',
-  'border border-light-1 dark:border-dark-3',
-  'first:border-t-0 last:border-b-0',
-);
+export const BaseOption = clsx('flex items-center flex-1', 'w-full', 'first:border-t-0 last:border-b-0');
 
 export const EmptyOption = clsx(BaseOption, 'text-dark-4');
 
-export const Option = clsx(
-  BaseOption,
-  'border border-light-2 dark:border-dark-8',
-  'hover:bg-light-1 dark:hover:bg-dark-7',
-  'cursor-pointer',
-);
+export const Option = clsx(BaseOption, 'hover:bg-cream-2 dark:hover:bg-space-2', 'cursor-pointer');
