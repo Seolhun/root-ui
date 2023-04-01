0bcaf39fac540b51ca0d564ba8ec2a0142facd08
Seolhun <shun10114@gmail.com>
2023-04-02 05:11:09 +0900

    chore: fix build publish

--- .rollup.cache/Users/hunseol/git/root-ui/components/atomics/grid/Container.d.ts
+++ .rollup.cache/Users/hunseol/git/root-ui/components/atomics/grid/Container.d.ts
@@ -3,6 +3,5 @@ type ElementType = HTMLDivElement;
 type ElementProps = React.HTMLAttributes<ElementType>;
 export interface ContainerProps extends ElementProps {
 }
-declare const Container: React.ForwardRefExoticComponent<ContainerProps & React.RefAttributes<HTMLDivElement>>;
-export { Container };
-export default Container;
+export declare const Container: React.ForwardRefExoticComponent<ContainerProps & React.RefAttributes<HTMLDivElement>>;
+export {};
