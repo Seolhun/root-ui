9b4ba55ee656fad186ccba487b7144e05256dc14
Seolhun <shun10114@gmail.com>
2023-04-16 19:58:03 +0900

    refactor(storybook): change storybook from 6 to 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,43 +1,33 @@
-const { resolve } = require('path');
+const {
+  resolve
+} = require('path');
 const context = resolve(__dirname, '../src');
-
 module.exports = {
-  stories: [
-    '../src/**/*.stories.@(js|jsx|ts|tsx)'
-  ],
-  core: {
-    builder: 'webpack5',
-  },
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-controls',
-    '@storybook/addon-viewport',
-    '@storybook/addon-actions',
-  ],
-  webpackFinal: async (config) => {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-controls', '@storybook/addon-viewport', '@storybook/addon-actions', '@storybook/addon-mdx-gfm'],
+  webpackFinal: async config => {
     config.resolve.alias = {
-      '~': context,
-    }
+      '~': context
+    };
     config.devServer = {
-      hot: true,
-    }
+      hot: true
+    };
     config.context = context;
-    config.module.rules.push(
-      {
-        test: [/\.[jt]sx?$/],
-        use: ['babel-loader'],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
-        ],
-      },
-    )
+    config.module.rules.push({
+      test: [/\.[jt]sx?$/],
+      use: ['babel-loader'],
+      exclude: /node_modules/
+    }, {
+      test: /\.s[ac]ss$/i,
+      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+    });
     return config;
   },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
