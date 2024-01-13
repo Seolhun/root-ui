e67979601572bfb9f1d4caccc695044166d5cb98
Seolhun <shun10114@gmail.com>
2024-01-13 12:17:36 +0000

    Deploying to storybook from @ Seolhun/root-ui@6f3ce5c158d47e863686c75cef8100c551c025c0 🚀

---
+++ sb-addons/storybook-4/manager-bundle.js
@@ -0,0 +1,3 @@
+try{
+(()=>{var h=__STORYBOOK_ADDONS__,{addons:t,types:f,mockChannel:c}=__STORYBOOK_ADDONS__;t.setConfig({isFullscreen:!1,showNav:!0,showPanel:!0,panelPosition:"right",enableShortcuts:!0,isToolshown:!0,theme:void 0,selectedPanel:void 0,initialActive:"sidebar",sidebar:{showRoots:!1,collapsedRoots:["other"]},toolbar:{title:{hidden:!1},zoom:{hidden:!1},eject:{hidden:!1},copy:{hidden:!1},fullscreen:{hidden:!1}}});})();
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
--- sb-addons/storybook-4/manager-bundle.mjs
+++
@@ -1,4 +0,0 @@
-try{
-var f=__STORYBOOKADDONS__,{addons:t,types:h,mockChannel:c}=__STORYBOOKADDONS__;t.setConfig({isFullscreen:!1,showNav:!0,showPanel:!0,panelPosition:"bottom",enableShortcuts:!0,isToolshown:!0,theme:void 0,selectedPanel:void 0,initialActive:"sidebar",sidebar:{showRoots:!1,collapsedRoots:["other"]},toolbar:{title:{hidden:!1},zoom:{hidden:!1},eject:{hidden:!1},copy:{hidden:!1},fullscreen:{hidden:!1}}});
-}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
-//# sourceMappingURL=manager-bundle.mjs.map
