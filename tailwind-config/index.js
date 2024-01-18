df1c09f1401528a471e44044593202771fae8972
Seolhun <shun10114@gmail.com>
2024-01-19 04:51:57 +0900

    refactor: update scale plugin system to use tailwind rules

--- tailwind-config/index.js
+++ tailwind-config/index.js
@@ -1,12 +1,12 @@
 module.exports = [
-  require('@tailwindcss/line-clamp'),
   require('@tailwindcss/aspect-ratio'),
   /**
    * theme is positioned first to allow other plugins to override
    */
   require('./RootUI.tailwind.config'),
+  require('./plugins/line-clamp'),
+  require('./plugins/scales'),
   require('./plugins/base'),
   require('./plugins/variants'),
   require('./plugins/components'),
-  require('./plugins/utilities'),
 ];
