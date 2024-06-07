ddcf547a6b33985ebf99f807823d928583febdb6
Seolhun <shun10114@gmail.com>
2024-06-07 20:57:26 +0900

    chore: update tsup

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,6 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   moduleNameMapper: {
     '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$': 'identity-obj-proxy',
-    '^~/(.*)': '<rootDir>/src/$1'
   },
   setupFilesAfterEnv: ['<rootDir>/test/setUpTest.ts']
 }
