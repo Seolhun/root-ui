5fbaca194d695f0dadbbd28b0cbd86a12ddd74ec
Seolhun <shun10114@gmail.com>
2024-07-20 17:20:29 +0900

    chore: remove lowercase disclosure

--- packages/components/src/components/combinations/disclosure/Disclosure.tsx
+++
@@ -1,8 +0,0 @@
-import { DisclosureButton } from './Disclosure.Button';
-import { DisclosurePanel } from './Disclosure.Panel';
-import { DisclosureRoot } from './Disclosure.Root';
-
-export const Disclosure = Object.assign(DisclosureRoot, {
-  Button: DisclosureButton,
-  Panel: DisclosurePanel,
-});
